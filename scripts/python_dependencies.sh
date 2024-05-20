80cb1608e3c6086b06f9abf2e3950069de0e07a3
Ishan942 <ishansirdeshpande@gmail.com>
2024-05-20 12:53:41 +0530

    third buidspec changed

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,4 +2,7 @@
 
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
+
+pip install --upgrade pip setuptools wheel
+pip install numpy cython
 pip install -r /home/ubuntu/FinalWebsite/requirements.txt
