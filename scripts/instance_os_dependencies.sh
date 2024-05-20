f91aa3087751a2dc8a2ce2ab644148c82c0a35bf
Ishan942 <ishansirdeshpande@gmail.com>
2024-05-20 14:39:15 +0530

    fourth

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install -y python3-pip
+sudo apt install -y python3.10 python3.10-venv python3.10-dev python3-pip nginx
 sudo apt install -y nginx
 sudo apt install -y virtualenv
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 virtualenv /home/ubuntu/env
+python3.10 -m venv venv
 source /home/ubuntu/env/bin/activate
-
-pip install --upgrade pip setuptools wheel
-pip install numpy cython
+pip install --upgrade pip
 pip install -r /home/ubuntu/FinalWebsite/requirements.txt
